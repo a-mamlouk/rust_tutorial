dc12e18f7ab1766ee2493309de80a91ffe8a9ba2
Anas <anas.mamlouk@yahoo.fr>
2025-11-13 21:51:01 +0100

    my 1st commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+println!("Hello, world!")
+let x = 5;
+}
+
+//cargo init
+//cargo build
+//cargo run
+//rustfmt main.rs
+
+// This is a simple Rust program that prints "Hello, world!" to the console.
+// commands used to run this program:
+// 1. Save the code in a file named `main.rs` inside a folder named
+// 2. Open a terminal and navigate to the folder containing `main.rs`.
+// 3. Run the command `rustc main.rs` to compile the program.
+// 4. After compilation, an executable file named `main` (or `main.exe` on Windows) will be created in the same folder.
+// 5. Run the executable by typing `./main` (or `main.exe` on Windows) in the terminal.
+// 6. You should see the output: `Hello, world!` `src`.
+//// Note: Make sure you have Rust installed on your system to compile and run the program.
